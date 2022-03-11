9a5091ea640d6d3fb0af93dd20eec7eab993d24e
junickaquino <junickaquino29@gmail.com>
2022-03-12 01:24:49 +0800

    Fireship - NodeJS - Hello World

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+
+console.log(global.luckyNum);
+global.luckyNum = "23";
+console.log(global.luckyNum);
+
+console.log(process.platform);
+console.log(process.env.USER);
+
+// ==========================
