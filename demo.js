e4b8bbacb8fc911e7418a3113e397164c55f930c
junickaquino <junickaquino29@gmail.com>
2022-03-12 10:20:12 +0800

    LinkedIn - Alexander Zanfir - 1 - Access directories

--- demo.js
+++ demo.js
@@ -1,9 +1,5 @@
 var fs = require("fs");
-var data = require("./data.json");
 
-console.log(data.name);
-
-fs.readFile("./data.json", "utf-8", function (err, data) {
-  var data = JSON.parse(data);
-  console.log(data.name);
+fs.readdir("c:/", (err, data) => {
+  console.log(data);
 });
